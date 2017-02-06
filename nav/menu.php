8a08d7f8ba03e6c7a101117024911b852301ac6f
Jamie Locatis <jlocatis@gmail.com>
2017-02-05 22:28:51 -0600

    Added comments to menu.php explaining how it works.

--- nav/menu.php
+++ nav/menu.php
@@ -1,7 +1,7 @@
 <div>
 
 <?php
-// Define each name associated with an URL
+// Define each page and it's associated URL.
 $pages = array(
 	'Home' => '/',
 	'Area Parks' => '/areaparks.php',
@@ -9,6 +9,7 @@ $pages = array(
 	'About Me' => '/about.php'
 );
 
+// Use a loop to go through the above array and create a link in the menu for each. If the current page is equal to // the page, the "current page" class will be added to let the user know they are currently on that page.
 foreach ($pages as $page => $url) {
 	print '<a href="'.$url.'"'.(($currentPage === $page) ? ' class ="currentpage" ': '').'>'.$page.'</a><br>';
 	}
